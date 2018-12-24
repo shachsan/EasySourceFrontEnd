c67a0cbe8c270ed13c5e4e8a3c86c9af051e8a05
Sanjay Sai <shachsan@hotmail.com>
2018-12-24 01:00:31 -0500

    work on show all products

--- javascript/index.js
+++ javascript/index.js
@@ -86,21 +86,41 @@ function fetchAllProducts(){
 }
 
 function showAllProducts(products){
-    // const divProductDetail = document.createElement('div')
     
     const divProduct = document.createElement('div');
-    divProduct.setAttribute("class","card-deck")
+    // divProduct.setAttribute("class","card-deck")
     for(let product of products){
         const divCard = document.createElement('div');
+        const divProductDetail = document.createElement('div')
+        divProductDetail.style.display="inline-block";
         // divCard.setAttribute("class", "card");//uncomment for imgCard1
-        divCard.setAttribute("class", "card")//comment out for imgCard1
-        divCard.style.width = "18rem";//comment out for imgCard1
-        divCard.innerHTML = imgCard(product.img_url);
+        // divCard.setAttribute("class", "card")//comment out for imgCard1
+        // divCard.style.width = "18rem";//comment out for imgCard1
+        // divCard.innerHTML = imgCard1(product.img_url);
         // divProduct.setAttribute("class", "card")
-        // const divImg = document.createElement('div');
-        // // divImg.setAttribute("class", "img-thumnail")
-        // divImg.innerHTML=`<img src=${product.img_url} class="img-thumbnail">`
-        // divProduct.append(divImg);
+        let divVendors;
+        const ul=document.createElement('ul')
+        for(let key in product){
+            if(key !== 'id' && key !=='img_url' && key!=='vendor_products'){
+                let li = document.createElement('li');
+                li.innerHTML=`<p>${key}:  ${product[key]}</p>`
+                ul.append(li);
+            }
+            if (key === 'vendor_products'){
+                divVendors=showVendorDetail(product[key]);
+                divVendors.style.display="inline-block";
+
+            }
+            
+        }
+        divProductDetail.append(ul);
+        
+        const divImg = document.createElement('div');
+        divImg.style.display="inline-block";
+        divImg.innerHTML=`<img src=${product.img_url} class="img-thumbnail" width="200px" height="180px">`
+        divCard.append(divImg);
+        divCard.append(divProductDetail);
+        divCard.append(divVendors);
         // let card=imgCard(product.img_url);
         // console.log(typeof(card));
         divProduct.appendChild(divCard);
@@ -111,25 +131,46 @@ function showAllProducts(products){
 
 }
 
-function imgCard(img_url){
 
-    const card = 
-        `<img class="card-img-top" src="${img_url}" alt="Card image cap">`+
-        `<div class="card-body">`+
-        `<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>`+
-        `</div>`
-    return card;
+function showVendorDetail(vendorDetail){
+    const div = document.createElement('div');
+    for(let vendor of vendorDetail){
+        const ul=document.createElement('ul');
+        ul.style.display="inline-block";
+        for(let key in vendor){
+            if(key !=='id' && key!=='vendor_id'){
+                let li=document.createElement('li');
+                li.innerHTML=`<p>${key}:  ${vendor[key]}</p>`
+                ul.append(li);
+            }
+        }
+        const orderButton=document.createElement('button');
+        orderButton.innerText="Order";
+        ul.append(orderButton);
+        div.append(ul);
+    }
+    return div;
 }
 
-function imgCard1(img_url){
-   const card = 
-        `<img class="card-img-top" src="${img_url}" alt="Card image cap">`+
-        `<div class="card-body">`+
-        `<h5 class="card-title">Card title</h5>`+
-        `<p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>`+
-        `</div>`+
-        `<div class="card-footer">`+
-        `<small class="text-muted">Last updated 3 mins ago</small>`+
-        `</div>`
-    return card;
-}
+// function imgCard(img_url){
+
+//     const card = 
+//         `<img class="card-img-top" src="${img_url}" alt="Card image cap">`+
+//         '<div class="card-body">'+
+//         '<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card content.</p>'+
+//         '</div>'
+//     return card;
+// }
+
+// function imgCard1(img_url){
+//    const card = 
+//         `<img class="card-img-top" src="${img_url}" alt="Card image cap">`+
+//         `<div class="card-body">`+
+//         `<h5 class="card-title">Card title</h5>`+
+//         `<p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>`+
+//         `</div>`+
+//         `<div class="card-footer">`+
+//         `<small class="text-muted">Last updated 3 mins ago</small>`+
+//         `</div>`
+//     return card;
+// }
